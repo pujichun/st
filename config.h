d2a59cc13545d1ecf68fece31ef3b5e7f34df041
pujichun <pujichun@outlook.com>
2021-02-17 15:47:54 +0800

    scrollback

--- config.h
+++ config.h
@@ -204,6 +204,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY,               XK_i,           kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_k,           kscrolldown,    {.i =  1} },
+	{ MODKEY|ControlMask,   XK_i,           kscrollup,      {.i = -1} },
+	{ MODKEY|ControlMask,   XK_k,           kscrolldown,    {.i = -1} },
 };
 
 /*
